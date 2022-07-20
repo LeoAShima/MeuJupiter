b701cccb8f9151fcd6892cf697a0363d28c32727
Leonardo Akira Shimabukuro <leonardoashimabukuro@gmail.com>
2022-07-20 01:42:39 +0000

    add credits command

--- main.py
+++ main.py
@@ -18,8 +18,15 @@ def historico(update, context):
     getHistorico()
     context.bot.send_document(chat_id=update.effective_chat.id, document=open('HistoricoEscolar.pdf', 'rb'))
 
+def acompCred(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Obtendo total de créditos...")
+    getAcompCred()
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('acompCred.png', 'rb'))
+
 historico_handler = CommandHandler('historico', historico)
+acompCred_handler = CommandHandler('acompCred', acompCred)
 dispatcher.add_handler(historico_handler)
+dispatcher.add_handler(acompCred_handler)
 
 updater.start_polling()
 
@@ -27,3 +34,8 @@ def getHistorico():
     print("Obtendo histórico escolar")
     os.system("wget --save-cookies cookies.txt --keep-session-cookies --post-data 'codpes="+os.environ.get('CODPES')+"&senusu="+os.environ.get('SENUSU')+"&Submit+Entrar+&url=' --delete-after https://uspdigital.usp.br/jupiterweb/autenticar")
     os.system("wget --load-cookies cookies.txt -O HistoricoEscolar.pdf 'https://uspdigital.usp.br/jupiterweb/historicoescolarListar?cmd=cmd&codpgmclg=1%2FR%2F3&codcurhab=3032%2F3170%2FAtivo'")
+
+def getAcompCred():
+    print("Obtendo total de créditos")
+    os.system("wget --save-cookies cookies.txt --keep-session-cookies --post-data 'codpes="+os.environ.get('CODPES')+"&senusu="+os.environ.get('SENUSU')+"&Submit+Entrar+&url=' --delete-after https://uspdigital.usp.br/jupiterweb/autenticar")
+    os.system("wget --load-cookies cookies.txt -O acompCred.png 'https://uspdigital.usp.br/jupiterweb/GraphicControle?tipografico=barGrupoDesempenho&codpgm=1&sequencia=0'")
