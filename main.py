37ee6c26fb6b8c2cde35c16888a870bb2f163235
Leonardo Akira Shimabukuro <leonardoashimabukuro@gmail.com>
2022-07-26 23:26:38 -0300

    add Periodo Ideal command

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import logging
+import re
+import yaml
 from telegram.ext import Updater, CommandHandler
+import telegram
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
@@ -23,12 +26,42 @@ def acompCred(update, context):
     getAcompCred()
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('acompCred.png', 'rb'))
 
+def periodoIdeal(update, context):
+    os.system("wget --save-cookies cookies.txt --keep-session-cookies --post-data 'codpes="+os.environ.get('CODPES')+"&senusu="+os.environ.get('SENUSU')+"&Submit+Entrar+&url=' --delete-after https://uspdigital.usp.br/jupiterweb/autenticar")
+    data = getPeriodoIdeal()
+    textoResposta = "<b>Aluno:</b> "+data[0].get("nompes")+"\n"
+    textoResposta += "<b>Curso:</b> "+data[0].get("nomcur")+"\n"
+    textoResposta += "<b>Ano ingresso:</b> "+str(data[0].get("anoing"))+"\n\n"
+    for periodo in data:
+        textoResposta += "<b>"+periodo.get("periodo")+"</b>\n"
+        textoResposta += "<b>Calculado em:</b> "+periodo.get("dtapcs")+"\n"
+        if periodo.get("semidllcn") != "0":
+            textoResposta += "<b>Período Ideal:</b> "+periodo.get("semidllcn")+"\n"
+        elif periodo.get("semidlmtr") != "0":
+            textoResposta += "<b>Período Ideal:</b> "+periodo.get("semidlmtr")+"º semestre\n"
+        elif periodo.get("semidlqdm") != "0":
+            textoResposta += "<b>Período Ideal:</b> "+periodo.get("semidlqdm")+"º quadrimestre\n"
+        textoResposta += "<b>Média ponderada, considerando as reprovações:</b> "+str(periodo.get("medpon"))+"\n"
+        textoResposta += "<b>Crédito acumulado:</b> "+str(periodo.get("totcrepgm"))+"\n\n"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=textoResposta, parse_mode=telegram.ParseMode.HTML)
+
+
 historico_handler = CommandHandler('historico', historico)
 acompCred_handler = CommandHandler('acompCred', acompCred)
+periodoIdeal_handler = CommandHandler('periodoIdeal', periodoIdeal)
 dispatcher.add_handler(historico_handler)
 dispatcher.add_handler(acompCred_handler)
+dispatcher.add_handler(periodoIdeal_handler)
 
 updater.start_polling()
+
+def getPeriodoIdeal():
+    result = os.popen("curl 'https://uspdigital.usp.br/jupiterweb/dwr/call/plaincall/PeriodoIdealControleDWR.listarPeriodoIdeal.dwr' --data-raw 'callCount=1\nc0-scriptName=PeriodoIdealControleDWR\nc0-methodName=listarPeriodoIdeal\nc0-id=0\nc0-param0=string:"+os.environ.get('CODPES')+"\nbatchId=2\ninstanceId=0\npage=\nscriptSessionId=\n' -b cookies.txt").read()
+    line = result.split("\n")[6]
+    dataString = re.search("\[(.*?)\]", line).group()
+    dataString = re.sub("(?<=:)(?!\s)", " ", dataString)
+    data = yaml.load(dataString, yaml.SafeLoader)
+    return data
 
 def getHistorico():
     print("Obtendo histórico escolar")
